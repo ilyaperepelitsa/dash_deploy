52919b274ff4357fa96a6f9e6ed46383ae449d1d
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-07-11 07:15:34 +0300

    Updated app.py.

--- app.py
+++ app.py
@@ -16,6 +16,6 @@ page_style = {'backgroundColor': colors['background'],
                             'marginRight' : 'auto',}
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = 'LSTM for predicting Power C'
+app.title = 'LSTM for predicting Power Consumption'
 server = app.server
 app.config.suppress_callback_exceptions = True
